9cac01c18473842b8e0a60e50def01ddb5cbb1e2
Amartya Mondal <hayyoulistentome@gmail.com>
2019-08-03 16:22:17 +0530

    feat: Improve id card uI

--- save_frontend.php
+++ save_frontend.php
@@ -1,22 +1,55 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-<title>Convert Div to image</title>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
+
+    <title>Document</title>
+
 </head>
 <body>
 
 
 <?php function save($name, $class, $dob, $phone, $father, $x){ ?>
-<!-- Here add image url after ajax response -->
-<div id="<?php echo $x?>" style="background: #EFFF96">
-	<h6>Name:<?php echo $name ?> </h6>
-	<h6>Class:<?php echo $class ?> </h6>
-	<h6>DoB:<?php echo $dob ?> </h6>
-	<h6>Phone:<?php echo $phone ?> </h6>
-	<h6>Father:<?php echo $father ?> </h6>
-</div>
+    <div class="container" style="background-color:#F6F8ED;height:68vh;width:45vh" id="<?php echo $x?>">
+        <div class="jumbotron jumbotron-fluid  text-white text-center"
+            style="background-color:white;width:300px;margin:-10px auto;height:60px;">
+
+            <div class="container">
+                <h1 class="display-1" style="font-size:18px;color:black;margin-top:-13px"><strong>ADHYAYAN NSS IIT PATNA <br>अध्ययन</strong>
+                </h1>
+                <hr style="width:210px;" />
+            </div>
+        </div>
+        <div class="row justify-content-sm-center justify-center">
+                <div class="photo"
+                    style="width:200px;height:250px;background-color:white;box-shadow: 10px 10px 20px;border:1px solid blue;margin-left:60px; margin-top:18px">
+                </div>
+        </div>
+        <br>
+
+        <div class="row justify-content-sm-center ">
+
+                <div style="margin-left: 20px">
+                    <form>
+                        <label style="font-size:12px;"><strong>Name: <?php echo $name ?></strong></label><br>
+                        <label style="font-size:12px;"><strong>Class: <?php echo $class ?></strong></label><br>
+                        <label style="font-size:12px;"><strong>DoB: <?php echo $dob ?></strong></label><br>
+                        <label style="font-size:12px;"><strong>Parent: <?php echo $father ?></strong></label><br>
+                        <label style="font-size:12px;"><strong>Phone: <?php echo $phone ?> </strong></label>
+                    </form>
+                </div>
+        </div>
+    </div>
+    <br><br>
 
 <script>
 	html2canvas([document.getElementById('<?php echo $x?>')], {
@@ -43,5 +76,9 @@
 <?php 
 }
 ?>
+    <!-- Optional JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
